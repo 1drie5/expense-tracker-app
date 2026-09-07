6149dd2154d137734071fcc695aa7b5c3a83e371
Idries Ahamed K A <idriesahamedka@gmail.com>
2026-09-07 21:01:18 +0530

    set up nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,5 @@
 import { Text, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Initial Setup</Text>
-    </View>
-  );
+  
 }
