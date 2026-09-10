e9644e1df47082fb49f7d7d28b07db43db4cae16
Idries Ahamed K A <idriesahamedka@gmail.com>
2026-09-10 23:52:15 +0530

    copied authentication with minimal understanding

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,10 +1,32 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { useAuth, useUser } from "@clerk/expo";
+import { useRouter } from "expo-router";
+import React from "react";
+import { Alert, Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function ProfileScreen() {
+  const { user } = useUser();
+  const { signOut } = useAuth();
+  const router = useRouter();
+  
+  const handleSignOut = () => {
+    Alert.alert("Sign out", "Are you sure you want to sign out?", [
+      { text: "Cancel", style: "cancel" },
+      {
+        text: "Sign out",
+        style: "destructive",
+        onPress: async () => {
+          await signOut();
+          router.replace("/sign-in");
+        },
+      },
+    ]);
+  };
   return (
-    <View>
-      <Text>Profile Screen</Text>
-    </View>
-  )
+    <SafeAreaView className="flex-1 bg-brand-body" edges={["top"]}>
+      <TouchableOpacity>
+        <Text onPress={handleSignOut}>Log Out</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
 }
