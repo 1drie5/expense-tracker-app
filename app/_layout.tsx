d9b9b9640f0bcd692bb0467448206f9b2c0d82a8
Idries Ahamed K A <idriesahamedka@gmail.com>
2026-09-14 20:53:13 +0530

    added React Query, still querying my own understanding

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,5 @@
+import { queryClient } from "@/lib/query/client";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { FlatList, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Animated, { FadeInDown } from "react-native-reanimated";
@@ -14,8 +16,10 @@ if (!publishableKey) {
 
 export default function RootLayout() {
   return (
-    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
-      <Slot />
-    </ClerkProvider>
+     <QueryClientProvider client={queryClient}>
+      <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+        <Slot />
+      </ClerkProvider>
+    </QueryClientProvider>
   );
 }
